bbb100b1b0e954e3ce0684a097b55eec04fcc78e
hanyibo <hanyibo@A800>
2025-11-18 10:18:15 +0800

    v1 async engine add replay mode

--- vllm/v1/engine/async_llm.py
+++ vllm/v1/engine/async_llm.py
@@ -294,7 +294,6 @@ class AsyncLLM(EngineClient):
                     raise ValueError("Replay generation is only supported for sampling requests.")
                 replay_tokens = self.tokenizer.encode(replay_output, lora_request)
                 replay_eos_token = self.tokenizer.get_lora_tokenizer(lora_request).eos_token_id
-                print(f"||| replay_output: {replay_output}, {replay_tokens}, {replay_eos_token}")
                 params.extra_args["replay_token_ids"] = replay_tokens
                 params.extra_args["replay_stop_token_id"] = replay_eos_token
 
